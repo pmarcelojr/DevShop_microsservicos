0f4792e54ec2da4825588b1c09df4f3da7f3e19d
pmarcelojr <marcelosantostecnologia@gmail.com>
2022-01-11 19:46:29 -0300

    criando web API para ser executada na AWS Lambda

---
+++ Cadastrador/Controllers/PedidoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Compartilhado;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace Cadastrador.Controllers
+{
+    [Route("api/[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        [HttpPost]
+        public async Task PostAsync([FromBody] Pedido pedido)
+        {
+            pedido.id = Guid.NewGuid().ToString();
+            pedido.DataDeCriacao = DateTime.UtcNow;
+
+            await pedido.SalvarAsync();
+
+            Console.WriteLine($"Pedido salvo com sucesso: Id {pedido.id}");
+        }
+    }
+}
---
+++ Compartilhado/Model/Pagamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Pagamento
+    {
+        public string NumeroDoCartao { get; set; }
+        public string Validade { get; set; }
+        public string CVV { get; set; }
+    }
+}
